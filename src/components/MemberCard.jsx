b0285778e5070acc8fe1a3240e92b80a00645cab
farhanisty <farhannivtasija@gmail.com>
2026-07-24 23:27:37 +0700

    fix image in member

--- src/components/MemberCard.jsx
+++ src/components/MemberCard.jsx
@@ -5,11 +5,33 @@ export default function MemberCard({ member }) {
     <>
       <div className="team-card reveal">
         <div className="team-avatar">
-          <img src={member.avatar} alt="" />
+          <MemberProfile member={member} />
         </div>
         <h4>{member.name}</h4>
         <span>{member.nim}</span>
       </div>
     </>
   )
+}
+
+function MemberProfile({ member }) {
+  if (member.id === 4) {
+    return <img style={{ transform: "translateY(35px)" }} src={member.avatar} alt="" />
+  } else if (member.id === 2) {
+    return <img style={{ transform: "translateY(45px)" }} src={member.avatar} alt="" />
+  } else if (member.id === 3) {
+    return <img style={{ transform: "translateY(35px)" }} src={member.avatar} alt="" />
+  } else if (member.id === 5) {
+    return <img style={{ transform: "translateY(35px)" }} src={member.avatar} alt="" />
+  } else if (member.id === 6) {
+    return <img style={{ transform: "translateY(35px)" }} src={member.avatar} alt="" />
+  } else if (member.id === 7) {
+    return <img style={{ transform: "translateY(35px)" }} src={member.avatar} alt="" />
+  } else if (member.id === 8) {
+    return <img style={{ transform: "translateY(35px)" }} src={member.avatar} alt="" />
+  } else if (member.id === 9) {
+    return <img style={{ transform: "translateY(35px)" }} src={member.avatar} alt="" />
+  }
+
+  return <img src={member.avatar} alt="" />
 }
